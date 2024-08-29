51a57ab1ad762986ad9911990ebc3057da8c1ee8
Alistair McCann <allyjmccann@gmail.com>
2024-08-29 14:31:46 +0200

    added an insults app with serverless and tested

---
+++ insults-api/functions/PostInsults/index.js
@@ -0,0 +1,16 @@
+const { sendResponse } = require('../../responses/index.js')
+
+const insults = [
+  "Yer maw’s got a face like a bulldog chewing a wasp.",
+  "You’ve got the charm of a damp sock.",
+  "Yer as welcome as a fart in a spacesuit.",
+  "You’re so dense, light bends around you.",
+  "If brains were dynamite, you wouldn’t have enough to blow yer nose."
+]
+
+exports.handler = async (event) => {
+const insult = JSON.parse(event.body);
+insults.push(insult);
+
+  return sendResponse(200, insults);
+};
